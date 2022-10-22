769a4157debe5b1c607b5c08994a2fe0b165faea
vince-exe <vincenzocaliendoantonio@gmail.com>
2022-10-22 23:00:22 +0200

    update
    updated the back-end, registration controller and router

---
+++ BackEnd/controllers/registrationController.js
@@ -0,0 +1,28 @@
+const usersDB = require('../data/users.json')
+const path = require('path')
+const fs = require('fs')
+
+const handleRegistration = (req, resp) => {
+    const credentials = {
+        id: usersDB[usersDB.length - 1].id + 1,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        pwd: req.body.pwd,
+        bdayDate: req.body.bdayDate
+    }
+
+    /* check for conflicts */
+    if (usersDB.find(it => it.email == credentials.email)) {
+        resp.sendStatus(409)
+        return
+    }
+
+    usersDB.push(credentials)
+    fs.writeFileSync(path.join(__dirname, '../data', 'users.json'), JSON.stringify(usersDB))
+
+    resp.sendStatus(200)
+}
+
+
+module.exports = { handleRegistration }
--- BackEnd/routers/registrationRouter.js
+++ BackEnd/routers/registrationRouter.js
@@ -1,10 +1,8 @@
 const express = require('express')
-const path = require('path')
+const registrationController = require('../controllers/registrationController')
 
 const router = express.Router()
 
-router.get('/', (req, resp) => {
-    
-})
+router.post('/', registrationController.handleRegistration)
 
 module.exports = router
